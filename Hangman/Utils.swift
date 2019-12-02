889d20c85958eed31c3775cd2a9931c1be1f2fb2
Juan Manuel Díaz Rivas <juanmdiazrivas95@gmail.com>
2019-12-02 22:51:24 +0100

    Keyboard will now an external component. The base application now have more decent MVP pattern

---
+++ Hangman/Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Utils.swift
+//  Hangman
+//
+//  Created by Plexus on 05/12/2019.
+//  Copyright © 2019 Plexus. All rights reserved.
+//
+
+import Foundation
+
+public class Utils {
+    public enum PlayedResult{
+        case noChanged,
+        used,
+        failed,
+        win,
+        lose
+    }
+    
+    public static var errorsOnInitAllowed: Float = 7
+}
